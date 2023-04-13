1271da195974929ae1d418f4caf5db9b9cd666b1
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-13 19:46:20 +0900

    git 업로드 연습

---
+++ src/com/KoreaIT/jave/WD/Main.java
@@ -0,0 +1,32 @@
+package com.KoreaIT.jave.WD;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("==프로그램 시작==");
+		
+		String cmd = sc.nextLine();
+//		int cmd = sc.nextInt();
+//		sc.next();
+		
+		System.out.println(cmd);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("==프로그램 끝==");
+		
+	}
+}
